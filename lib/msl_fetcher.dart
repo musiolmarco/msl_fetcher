61e450d20a4b2a30ed3a9918aed40951ee4dcb59
Marco Musiol <0203marcom@gmail.com>
2023-06-01 20:39:55 +0200

    Setup cubit, states and MslFetcher widget

--- lib/msl_fetcher.dart
+++ lib/msl_fetcher.dart
@@ -1,2 +1,48 @@
 library msl_fetcher;
 
+import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:msl_fetcher/msl_fetcher_provider.dart';
+
+/// The [MslFetcher] is a [Widget] that setup the whole fetching progress
+///
+/// Use this [Widget] to fetch data and handle all states in under a minute
+class MslFetcher<T> extends StatelessWidget {
+  /// The method that fetches and returns the data
+  final Future<T> Function() onDataFetched;
+
+  /// This [Widget] is displayed while the data is being fetched
+  final Widget loadingWidget;
+
+  /// This [Widget] is displayed if the data is available
+  final Widget dataAvailableWidget;
+
+  /// Tis [Widget] is displayed if there was an error while fetching the data
+  final Widget fetchingErrorWidget;
+
+  const MslFetcher({
+    super.key,
+    required this.onDataFetched,
+    required this.dataAvailableWidget,
+    required this.fetchingErrorWidget,
+    required this.loadingWidget,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_) => MslFetcherProvider(onDataFetched: onDataFetched),
+      child: BlocBuilder<MslFetcherProvider, MslFetcherProviderState>(
+        builder: (context, state) {
+          if (state is MslFetcherProviderLoading) {
+            return loadingWidget;
+          } else if (state is MslFetcherProviderDataAvailable) {
+            return dataAvailableWidget;
+          } else {
+            return fetchingErrorWidget;
+          }
+        },
+      ),
+    );
+  }
+}
---
+++ lib/msl_fetcher_provider.dart
@@ -0,0 +1,36 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:meta/meta.dart';
+
+part 'msl_fetcher_provider_state.dart';
+
+class MslFetcherProvider<T> extends Cubit<MslFetcherProviderState> {
+  /// The method that fetches and returns the data
+  final Future<T> Function() onDataFetched;
+
+  MslFetcherProvider({
+    required this.onDataFetched,
+  }) : super(MslFetcherProviderLoading());
+
+  /// Fetch the data from the given method and emit the specific state
+  ///
+  /// Emit [MslFetcherProviderLoading] while the data is being fetched
+  /// Emit [MslFetcherProviderDataAvailable] if the data is available
+  /// Emit [MslFetcherProviderError] if there is an error within the process
+  Future<void> fetchDataAndEmitState() async {
+    emit(MslFetcherProviderLoading());
+
+    try {
+      T data = await onDataFetched();
+      emit(
+        MslFetcherProviderDataAvailable(
+          data: data,
+        ),
+      );
+    } catch (e) {
+      emit(
+        MslFetcherProviderError(),
+      );
+    }
+  }
+}
