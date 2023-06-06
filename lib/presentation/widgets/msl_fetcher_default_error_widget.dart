707eb8bfd069cc4401b9688909418b18d8ff40ca
Marco Musiol <0203marcom@gmail.com>
2023-06-06 15:44:26 +0200

    Created default widget values

---
+++ lib/presentation/widgets/msl_fetcher_default_error_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MslFetcherDefaultErrorWidget extends StatelessWidget {
+  const MslFetcherDefaultErrorWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
